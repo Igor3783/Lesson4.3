d0e3a2e89fb3a067d99912faea497dada40ac11f
Igor <monaxnew@rambler.ru>
2023-02-12 13:37:58 +0300

    add task3

---
+++ Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int InputInt(string messag)
+{
+    Console.WriteLine(messag);
+    return Convert.ToInt32(Console.ReadLine());
+
+}
+int[]GenerateArray(int len){
+
+    int[] array =new int [len];
+    Random rnd = new Random();
+    for(int i =0; i< array.Length; i++){
+        array[i] = rnd.Next(0, 10);
+    }
+    return array;
+}
+
+void PrintArray( int[] array){
+
+    foreach( int item in array){
+        Console.Write($"{item}\t");
+    }
+}
+
+int sizeOfArray =InputInt("Введите размер массива: ");
+int[] array = GenerateArray(sizeOfArray);
+PrintArray(array);
